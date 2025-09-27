5e1e5dff083b67b18480360a600fc55ae76079fb
Elissandro Guedes <edossantosguedes@gmail.com>
2025-09-27 11:09:01 -0300

    DTOs

--- backend/aula/src/main/java/com/devsuperior/aula/resources/CategoryResource.java
+++ backend/aula/src/main/java/com/devsuperior/aula/resources/CategoryResource.java
@@ -1,15 +1,15 @@
 package com.devsuperior.aula.resources;
 
 import com.devsuperior.aula.entities.Category;
+import com.devsuperior.aula.entities.CategoryDTO;
 import com.devsuperior.aula.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -19,8 +19,9 @@ public class CategoryResource {
     private CategoryRepository categoryRepository;
 
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
+    public ResponseEntity<List<CategoryDTO>> findAll() {
         List<Category> list = categoryRepository.findAll();
-        return ResponseEntity.ok().body(list);
+        List<CategoryDTO> dtos = list.stream().map(CategoryDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtos);
     }
 }
