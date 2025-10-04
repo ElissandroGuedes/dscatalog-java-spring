4bc58ca425299b288ba064882800f223b0f4ef5d
Elissandro Guedes <edossantosguedes@gmail.com>
2025-10-04 09:04:52 -0300

    Auditing data

--- backend/aula/src/main/java/com/devsuperior/aula/entities/Category.java
+++ backend/aula/src/main/java/com/devsuperior/aula/entities/Category.java
@@ -1,9 +1,8 @@
 package com.devsuperior.aula.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.Instant;
 
 @Entity(name = "tb_category")
 public class Category {
@@ -12,6 +11,12 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
     public Category() {
     }
@@ -34,6 +39,23 @@ public class Category {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
     }
 
     @Override
